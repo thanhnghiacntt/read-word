0797af51afc6279fcfd451103915abf9ed3478bf
nghiant <nghiant@dekiconnect.com>
2019-08-27 20:44:12 +0700

    Add file

---
+++ TaoFileDoc/TaoFileDoc/ThanhNghiaCNTT/Com/Helper/TextContent.cs
@@ -0,0 +1,70 @@
+﻿namespace TaoFileDoc.ThanhNghiaCNTT.Com.Helper
+{
+    public static class TextContent
+    {
+        /// <summary>
+        /// CỘNG HOÀ XÃ HỘI CHỦ NGHĨA VIỆT NAM
+        /// </summary>
+        public static string CongHoa = "CỘNG HOÀ XÃ HỘI CHỦ NGHĨA VIỆT NAM";
+
+        /// <summary>
+        /// Độc lập-Tự do-Hạnh phúc
+        /// </summary>
+        public static string DocLap = "Độc lập-Tự do-Hạnh phúc";
+
+        /// <summary>
+        /// HỢP ĐỒNG CÔNG VIỆC
+        /// </summary>
+        public static string HopDong = "HỢP ĐỒNG CÔNG VIỆC";
+
+        /// <summary>
+        /// Số: {ContractNumber}/KHCN-{TitleCode}
+        /// </summary>
+        public static string SoHopDong = "Số: {ContractNumber}/KHCN-{TitleCode}";
+
+        /// <summary>
+        /// Bên A: {FullName}, chủ nhiệm đề tài \"{Title}\", {TitleCode}
+        /// </summary>
+        public static string BeA = "Bên A: {FullName}, chủ nhiệm đề tài \"{Title}\", {TitleCode}";
+
+        /// <summary>
+        /// Đơn vi: {WorkUnit}
+        /// </summary>
+        public static string DonViConTac = "Đơn vi: {WorkUnit}";
+
+        /// <summary>
+        /// CMND số: {Id}
+        /// </summary>
+        public static string CMND = "CMND số: {Id}";
+
+        /// <summary>
+        /// Ngày cấp: {DateId}
+        /// </summary>
+        public static string NgayCap = "Ngày cấp: {DateId}";
+
+        /// <summary>
+        /// Nơi cấp:{AddressId}
+        /// </summary>
+        public static string NoiCap = "Nơi cấp:{AddressId}";
+
+        /// <summary>
+        /// Mã số thuế: {TaxCode}
+        /// </summary>
+        public static string MaSoThue = "Mã số thuế: {TaxCode}";
+
+        /// <summary>
+        /// Điện thoại: {Phone}
+        /// </summary>
+        public static string DienThoai = "Điện thoại: {Phone}";
+
+        /// <summary>
+        /// Bên B: CÁC THÀNH VIÊN THAM GIA THỰC HIỆN ĐỀ TÀI GỒM:
+        /// </summary>
+        public static string BenB = "Bên B: CÁC THÀNH VIÊN THAM GIA THỰC HIỆN ĐỀ TÀI GỒM:";
+
+        /// <summary>
+        /// {STT}. {FullName} - {Title}
+        /// </summary>
+        public static string ConBenB = "{STT}. {FullName} - {Title}";
+    }
+}
